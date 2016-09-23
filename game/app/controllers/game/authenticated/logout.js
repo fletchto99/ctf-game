83eec03ad43d1c18b8e3de36ffb3b2606ed24b9b
Matt Langlois <fletchto99@gmail.com>
2016-09-23 18:09:45 -0400

    Minor bug fix for killing the session

--- game/app/controllers/game/authenticated/logout.js
+++ game/app/controllers/game/authenticated/logout.js
@@ -1,7 +1,12 @@
 module.exports =  {
     get(req, res) {
-        req.session.destroy(function (err) {
-            res.redirect("/");
-        });
+        //Prevent CSRF from destroying the user's session due to the weird cookie implementation I am using
+        if (req.cookies['admin_cookie'] && req.session.admin_cookie && req.cookies['admin_cookie'] == req.session.admin_cookie) {
+            res.redirect('/')
+        } else {
+            req.session.destroy(function (err) {
+                res.redirect("/");
+            });
+        }
     }
 };
