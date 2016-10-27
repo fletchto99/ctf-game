854a2ba799125af1e04d950a9bd8572f1068d83c
Matt Langlois <fletchto99@gmail.com>
2016-10-27 16:07:34 -0400

    Refactor db script

--- challenge 1/database/build.sql
+++ challenge 1/database/build.sql
@@ -11,3 +11,11 @@ CREATE TABLE Game_Users(
 	Admin_Last_Name VARCHAR(60) DEFAULT 'Job' NOT NULL,
 	Admin_Email VARCHAR(60) DEFAULT 'winner@ctf-game.xyz' NOT NULL
 );
+
+CREATE TABLE "session" (
+    "sid" varchar NOT NULL COLLATE "default",
+    "sess" json NOT NULL,
+	"expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
